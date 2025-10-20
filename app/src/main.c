d1b297d880d19676ac10d89c06d79ba21da81db3
Maury Mangaoang <maurymangaoang@gmail.com>
2025-10-19 18:56:07 -0600

    predetermined password

--- app/src/main.c
+++ app/src/main.c
@@ -10,7 +10,8 @@
 #define SLEEP_MS 1
 
 int main(void) {
-  int counter = 0;
+  int pw_array[] = {0, 0, 0};
+  int index = 0;
 
   if (0 > BTN_init()) {
     return 0;
@@ -20,112 +21,52 @@ int main(void) {
     return 0;
   }
 
+  LED_toggle(LED0);
 
   while(1) {
-    
-    if (BTN_check_clear_pressed(BTN0))
-      counter++;
-    
-    switch (counter) {
-      case 1:
-        LED_set(LED0, 1);
-        LED_set(LED1, 0);
-        LED_set(LED2, 0);
-        LED_set(LED3, 0);
-        break;
-      case 2:
-        LED_set(LED0, 0);
-        LED_set(LED1, 1);
-        LED_set(LED2, 0);
-        LED_set(LED3, 0);
-        break;
-      case 3:
-        LED_set(LED0, 1);
-        LED_set(LED1, 1);
-        LED_set(LED2, 0);
-        LED_set(LED3, 0);
-        break;
-      case 4:
-        LED_set(LED0, 0);
-        LED_set(LED1, 0);
-        LED_set(LED2, 1);
-        LED_set(LED3, 0);
-        break;
-      case 5:
-        LED_set(LED0, 1);
-        LED_set(LED1, 0);
-        LED_set(LED2, 1);
-        LED_set(LED3, 0);
-        break;
-      case 6:
-        LED_set(LED0, 0);
-        LED_set(LED1, 1);
-        LED_set(LED2, 1);
-        LED_set(LED3, 0);
-        break;
-      case 7:
-        LED_set(LED0, 1);
-        LED_set(LED1, 1);
-        LED_set(LED2, 1);
-        LED_set(LED3, 0);
-        break;
-      case 8:
-        LED_set(LED0, 0);
-        LED_set(LED1, 0);
-        LED_set(LED2, 0);
-        LED_set(LED3, 1);
-        break;
-      case 9:
-        LED_set(LED0, 1);
-        LED_set(LED1, 0);
-        LED_set(LED2, 0);
-        LED_set(LED3, 1);
-        break;
-      case 10:
-        LED_set(LED0, 0);
-        LED_set(LED1, 1);
-        LED_set(LED2, 0);
-        LED_set(LED3, 1);
-        break;
-      case 11:
-        LED_set(LED0, 1);
-        LED_set(LED1, 1);
-        LED_set(LED2, 0);
-        LED_set(LED3, 1);
-        break;
-      case 12:
-        LED_set(LED0, 0);
-        LED_set(LED1, 0);
-        LED_set(LED2, 1);
-        LED_set(LED3, 1);
-        break;
-      case 13:
-        LED_set(LED0, 1);
-        LED_set(LED1, 0);
-        LED_set(LED2, 1);
-        LED_set(LED3, 1);
-        break;
-      case 14:
-        LED_set(LED0, 0);
-        LED_set(LED1, 1);
-        LED_set(LED2, 1);
-        LED_set(LED3, 1);
-        break;
-      case 15:
-        LED_set(LED0, 1);
-        LED_set(LED1, 1);
-        LED_set(LED2, 1);
-        LED_set(LED3, 1);
-        break;
-      case 16:
-        LED_set(LED0, 0);
-        LED_set(LED1, 0);
-        LED_set(LED2, 0);
-        LED_set(LED3, 0);
-        counter = 0;
-        break;
-      default:
-        break;
+    if (BTN_check_clear_pressed(BTN0)) {
+      if (index == 0) 
+        pw_array[0] = 1;
+      index++;
     }
+    else if (BTN_check_clear_pressed(BTN1))
+    {
+      if (index == 1) 
+        pw_array[1] = 1;
+      index++;
+    }
+    else if (BTN_check_clear_pressed(BTN2))
+    {
+      if (index == 2) 
+        pw_array[2] = 1;
+      index++;
+    }
+    else if (BTN_check_clear_pressed(BTN3))
+    {
+      if (pw_array[0] == 1 && pw_array[1] == 1 && pw_array[2] == 1)
+        printk("Correct!\n");
+      else
+        printk("Incorrect!\n");
+
+      LED_toggle(LED0);
+      index = 0;
+      pw_array[0] = 0;
+      pw_array[1] = 0;
+      pw_array[2] = 0;
+
+      while (!(BTN_check_clear_pressed(BTN0) ||
+             BTN_check_clear_pressed(BTN1) ||
+             BTN_check_clear_pressed(BTN2) ||
+             BTN_check_clear_pressed(BTN3))) {
+        k_msleep(SLEEP_MS);
+      }
+
+      LED_toggle(LED0);
+    }
+    else;
+  
+  if (index > 3)
+    pw_array[0] = 0;
+
   }
 }
